3652635129b436052a58b96382857d6790c49e46
AhmedMaherFathy <ahmedmaher.xd8551@gmail.com>
2024-08-15 09:20:15 +0300

    show routes list

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return "view('welcome')";
@@ -8,4 +9,10 @@ Route::get('/', function () {
 
 Route::get('/home', function () {
     return "home page web work";
+});
+
+Route::get('/log-routes', function () {
+    $routes = Artisan::call('route:list');
+    $routes = Artisan::output();
+    return $routes;
 });
